ed8a320ed8cb8ff13aaa53e0666f6ce037ba1a88
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2023-08-23 00:09:28 +0300

    Utility functions

--- utils.c
+++ utils.c
@@ -15,7 +15,7 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ASCII in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
